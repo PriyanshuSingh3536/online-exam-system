10008c629fd321e6890cc41b659fb4c16e120505
priyanshu singh <priyanshusingh@priyanshus-MacBook-Air.local>
2026-07-16 17:58:33 +0530

    Fix admin routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,37 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./Login";
 import Register from "./Register";
+
 import StudentDashboard from "./StudentDashboard";
 import ExamPage from "./ExamPage";
 import ResultPage from "./ResultPage";
 import StudentResults from "./StudentResults";
 
-
+import AdminDashboard from "./AdminDashboard";
+import AdminExams from "./AdminExams";
+import AdminResults from "./AdminResults";
 
 function App() {
   return (
     <Router>
       <Routes>
+
+        {/* Authentication */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-       <Route path="/student" element={<StudentDashboard />} />
+
+        {/* Student Routes */}
+        <Route path="/student" element={<StudentDashboard />} />
         <Route path="/exam/:id" element={<ExamPage />} />
         <Route path="/result" element={<ResultPage />} />
         <Route path="/student/results" element={<StudentResults />} />
-      </Routes>
 
-      {/* Floating Chatbot */}
-     
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/exams" element={<AdminExams />} />
+        <Route path="/admin/results" element={<AdminResults />} />
+
+      </Routes>
     </Router>
   );
 }
